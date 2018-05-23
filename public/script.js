0af43c8c6165a3d4a204915abd0474756d099695
Jacob <jacobboisseau@gmail.com>
2018-05-23 13:40:50 -0400

    finished project

--- public/script.js
+++ public/script.js
@@ -1,16 +1,36 @@
 var PRICE = 9.99;
+var LOAD_MIN = 10;
 new Vue({
   el: "#app",
   data: {
     total: 0,
-    items: [
-      { id: 1, title: "item 1" },
-      { id: 2, title: "item 2" },
-      { id: 3, title: "item 3" }
-    ],
-    cart: []
+    items: [],
+    cart: [],
+    results: [],
+    newSearch: "simpsons",
+    lastSearch: "",
+    loading: false,
+    price: PRICE
   },
   methods: {
+    appendItems: function() {
+      if (this.items.length < this.results.length) {
+        var append = this.results.slice(this.items.length, this.items.length + LOAD_MIN);
+        this.items = this.items.concat(append);
+      }
+    },
+    onSubmit: function() {
+      if(this.newSearch.length){
+      this.items = [];
+      this.loading = true;
+      this.$http.get("/search/".concat(this.newSearch)).then(function(res) {
+        this.results = res.data;
+        this.appendItems();
+        this.lastSearch = this.newSearch;
+        this.loading = false;
+      });
+    }
+    },
     addItem: function(index) {
       var item = this.items[index];
       var found = false;
@@ -53,5 +73,19 @@ new Vue({
     currency: function(price) {
       return "$".concat(price.toFixed(2));
     }
+  },
+  computed:{
+    noMoreItems: function(){
+      return this.items.length === this.results.length && this.results.length > 0;
+    }
+  },
+  mounted: function() {
+    this.onSubmit();
+    var vueInstance = this;
+    var elem = document.getElementById('product-list-bottom');
+    var watcher = scrollMonitor.create(elem);
+    watcher.enterViewport(function() {
+      vueInstance.appendItems();
+    });
   }
 });
